ef3bc6f576f884174ea686adf3b29ab116e9814f
gmora08 <gmora008@gmail.com>
2019-09-19 11:16:47 -0500

    render basic live view

---
+++ lib/todo_app_web/live/todo_live.ex
@@ -0,0 +1,11 @@
+defmodule TodoAppWeb.TodoLive do
+  use Phoenix.LiveView
+
+  def mount(_session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"Rendering LiveView"
+  end
+end
